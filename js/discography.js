9cd260f5d0c6dd919596a3fd86cab483691fb2f0
flareucharts <kwoo7in@gmail.com>
2026-05-31 16:21:37 +0700

    Update discography.js

--- js/discography.js
+++ js/discography.js
@@ -1,96 +1,39 @@
-const sheetURL =
-"https://script.google.com/macros/s/AKFYCBxxxxxxxxxxxx/exec";
-
-fetch(sheetURL)
+fetch(API_URL)
 .then(res => res.json())
 .then(data => {
 
-  data.sort(
-    (a,b)=>
-    new Date(b.releaseDate)
-    -
-    new Date(a.releaseDate)
-  );
-
-  renderDiscography(data);
-
-  renderHomeDiscography(data);
-
-});
-
-function renderDiscography(data){
+  const albums = data.discography
+    .sort((a,b)=>
+      new Date(b.releaseDate) -
+      new Date(a.releaseDate)
+    );
 
   const container =
     document.getElementById("discography");
 
-  if(!container) return;
-
   container.innerHTML = "";
 
-  data.forEach(album => {
+  albums.forEach(album => {
 
     container.innerHTML += `
-
       <div class="album-card">
-
-        <img
-          src="${album.cover}"
-          alt="${album.title}"
-        >
+        <img src="${album.cover}">
 
         <div class="album-info">
+          <div class="album-title">
+            ${album.title}
+          </div>
 
-          <h3>${album.title}</h3>
+          <div class="album-artist">
+            ${album.artist}
+          </div>
 
-          <p>${album.artist}</p>
-
+          <div class="album-date">
+            ${album.releaseDate.replaceAll("-",".")}
+          </div>
         </div>
-
       </div>
-
     `;
-
   });
 
-}
-
-function renderHomeDiscography(data){
-
-  const container =
-    document.getElementById(
-      "homeDiscography"
-    );
-
-  if(!container) return;
-
-  const latestAlbums =
-    data.slice(0,3);
-
-  container.innerHTML = "";
-
-  latestAlbums.forEach(album => {
-
-    container.innerHTML += `
-
-      <div class="music-card">
-
-        <img
-          class="music-cover"
-          src="${album.cover}"
-        >
-
-        <div class="music-title">
-          ${album.title}
-        </div>
-
-        <div class="music-sub">
-          ${album.artist}
-        </div>
-
-      </div>
-
-    `;
-
-  });
-
-}
+});
