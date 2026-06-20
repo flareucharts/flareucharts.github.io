a197915cc60556dec6480fca66785222bf5a3f8c
flareucharts <kwoo7in@gmail.com>
2026-06-21 01:21:24 +0700

    Update discography.js

--- js/discography.js
+++ js/discography.js
@@ -1,57 +1,63 @@
 console.log("Discography JS Loaded");
 
-const API_URL =
-"https://script.google.com/macros/s/AKfycbwsYIjF8eJTi54N40LE7pfyT6ZJw5wGTNKK5_2j26qf6T3AsmljVklTc5CReCaz4FbvQg/exec";
+const albums = [
+  {
+    title: "YOUTH ERROR",
+    artist: "FLARE U",
+    releaseDate: "2026.05.13",
+    cover: "youth-error.png"
+  },
 
-fetch("https://script.google.com/macros/s/AKfycbwsYIjF8eJTi54N40LE7pfyT6ZJw5wGTNKK5_2j26qf6T3AsmljVklTc5CReCaz4FbvQg/exec")
-.then(res=>res.json())
-.then(data=>{
+{
+    title: "Sweet Dream (Japan Edition)",
+    artist: "CHUEI LI YU",
+    releaseDate: "2027.03.18",
+    cover: "sweet-dream-jp.png"
+  }
 
-  const container =
-    document.getElementById(
-      "discography"
-    );
+  {
+    title: "Sweet Dream",
+    artist: "CHUEI LI YU",
+    releaseDate: "2025.12.03",
+    cover: "sweet-dream.png"
+  }
 
-  const albums =
-    data.discography.sort(
-      (a,b)=>
-      new Date(b.releaseDate) -
-      new Date(a.releaseDate)
-    );
 
+];
+
+const container = document.getElementById("discography");
+
+function renderAlbums() {
   container.innerHTML = "";
 
-  albums.forEach(album=>{
+  albums.forEach(album => {
+    container.innerHTML += `
+      <div class="album-card">
 
-  container.innerHTML += `
-    <a
-      href="./album/?slug=${album.slug}"
-      class="album-card"
-    >
+        <img
+          src="../images/albums/${album.cover}"
+          alt="${album.title}"
+        >
 
-      <img
-        src="../images/albums/${album.cover}"
-        alt="${album.title}"
-      >
+        <div class="album-info">
 
-      <div class="album-info">
+          <div class="album-title">
+            ${album.title}
+          </div>
 
-        <div class="album-title">
-          ${album.title}
-        </div>
+          <div class="album-artist">
+            ${album.artist}
+          </div>
 
-        <div class="album-artist">
-          ${album.artist}
-        </div>
+          <div class="album-date">
+            ${album.releaseDate.replaceAll("-", ".")}
+          </div>
 
-        <div class="album-date">
-          ${album.releaseDate.replaceAll("-",".")}
         </div>
 
       </div>
+    `;
+  });
+}
 
-    </a>
-  `;
-
-});
-});
+renderAlbums();
