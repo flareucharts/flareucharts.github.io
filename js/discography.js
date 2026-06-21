21344da84c4e8ec297facc46b0c4c1d5a87ae1cb
flareucharts <kwoo7in@gmail.com>
2026-06-21 21:30:56 +0700

    Update discography.js

--- js/discography.js
+++ js/discography.js
@@ -1,45 +1,21 @@
 console.log("Discography JS Loaded");
 
-const formatDate = (d) => d.replaceAll("-", ".");
-
-const albums = [
-  {
-    slug: "youth-error",
-    title: "YOUTH ERROR",
-    artist: "FLARE U",
-    releaseDate: "2026-05-13",
-    cover: "youth-error.png"
-  },
-  {
-    slug: "sweet-dream-jp",
-    title: "Sweet Dream (Japan Edition)",
-    artist: "CHUEI LI YU",
-    releaseDate: "2027-03-18",
-    cover: "sweet-dream-jp.png"
-  },
-  {
-    slug: "sweet-dream",
-    title: "Sweet Dream",
-    artist: "CHUEI LI YU",
-    releaseDate: "2025-12-03",
-    cover: "sweet-dream.png"
-  }
-];
-
 const container = document.getElementById("discography");
+
+const sorted = [...ALBUMS]
+  .sort((a,b) => new Date(b.releaseDate) - new Date(a.releaseDate));
 
 container.innerHTML = "";
 
-albums.forEach(album => {
+sorted.forEach(album => {
   container.innerHTML += `
-    <a href="./album/?slug=${album.slug}" class="album-card">
+    <a href="./albums/${album.slug}" class="album-card">
 
-      <img src="../images/albums/${album.cover}" alt="${album.title}">
+      <img src="../images/albums/${album.cover}">
 
       <div class="album-info">
-        <div class="album-title">${album.title}</div>
         <div class="album-artist">${album.artist}</div>
-        <div class="album-date">${formatDate(album.releaseDate)}</div>
+        <div class="album-date">${album.releaseDate}</div>
       </div>
 
     </a>
