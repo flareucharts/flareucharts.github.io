6d824b23d363f4c071b0373bd64ebb3d2f8981f4
flareucharts <kwoo7in@gmail.com>
2026-06-22 04:41:31 +0700

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,52 +1,60 @@
 console.log("Data Loaded");
 
-// helper bikin slug otomatis
-function makeSlug(title) {
+function makeSlug(title){
   return title
     .toLowerCase()
-    .replace(/[^a-z0-9]+/g, "-")
-    .replace(/(^-|-$)/g, "");
+    .replace(/[^a-z0-9]+/g,"-")
+    .replace(/(^-|-$)/g,"");
 }
 
-window.ALBUMS = [
+window.discography = [
   {
+    albumType: "1st Mini Album",
     title: "YOUTH ERROR",
     artist: "FLARE U",
     releaseDate: "2026-05-13",
     cover: "youth-error.png",
-    tracklist: [
-      "WAY 2 U",
-      "Hyper",
-      "우니까 (Don't cry)",
-      "MIRACLE",
-      "WOO-HOO",
-      "놀이터 (Playground)"
-    ]
+    spotify: "",
+    apple: "",
+    youtubeMusic: "",
+    melon: "",
+    genie: "",
+    bugs: "",
+    tracks: "WAY 2 U|Hyper|우니까 (Don't cry)|MIRACLE|WOO-HOO|놀이터 (Playground)"
   },
+
   {
+    albumType: "Single Album",
     title: "Sweet Dream (Japan Edition)",
     artist: "CHUEI LI YU",
     releaseDate: "2026-03-18",
     cover: "sweet-dream-jp.png",
-    tracklist: [
-      "Flower pot"
-    ]
+    spotify: "",
+    apple: "",
+    youtubeMusic: "",
+    melon: "",
+    genie: "",
+    bugs: "",
+    tracks: "Flower pot"
   },
+
   {
+    albumType: "Single Album",
     title: "Sweet Dream",
     artist: "CHUEI LI YU",
     releaseDate: "2025-12-03",
     cover: "sweet-dream.png",
-    tracklist: [
-      "UxYOUxU",
-      "Fresh",
-      "Hello My Friend"
-    ]
+    spotify: "",
+    apple: "",
+    youtubeMusic: "",
+    melon: "",
+    genie: "",
+    bugs: "",
+    tracks: "UxYOUxU|Fresh|안녕 My Friend"
   }
 ];
 
-// inject slug otomatis ke semua album
-window.ALBUMS = window.ALBUMS.map(album => ({
+window.discography = window.discography.map(album => ({
   ...album,
   slug: makeSlug(album.title)
 }));
