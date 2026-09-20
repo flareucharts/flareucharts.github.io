54d4da82f11308c7441e827d680af3ca1be9006a
flareucharts <kwoo7in@gmail.com>
2026-09-20 19:00:29 +0700

    Update guide.js

--- js/guide.js
+++ js/guide.js
@@ -40,7 +40,7 @@ document.addEventListener("DOMContentLoaded", () => {
         "music-show": {
 
             pills: [
-                "Overview"
+                "Overview",
                 "Voting Schedule",
                 "The Show",
                 "Show Champion",
